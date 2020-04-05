90579f2bfa4f922121b4ab542075bbf82a57d4fe
hyoj <chohyojoy@gmail.com>
2020-04-05 23:54:50 +0900

    cryogen-core 라이브러리 디펜던시 적용
    1. 기존에 cryogen-core 를 소스를 커스텀해서 사용하려고 적용한 부분 제거
    
    2. cryogen-core "0.3.1" 적용
    (주의) Windows 에서 동작하지 않는 이슈가 있음

--- blog/project.clj
+++ blog/project.clj
@@ -8,23 +8,7 @@
                       [compojure "1.6.1"]
                       [ring-server "0.5.0"]
                       [cryogen-asciidoc "0.1.4"]
-                      
-                      ;;
-                      [camel-snake-kebab "0.4.0"]
-                      [cheshire "5.8.1"]
-                      [clj-rss "0.2.5"]
-                      [clj-text-decoration "0.0.3"]
-                      [enlive "1.1.6"]
-                      [hawk "0.2.11"]
-                      [hiccup "1.0.5"]
-                      [io.aviso/pretty "0.1.37"]
-                      [me.raynes/fs "1.4.6"]
-                      [pandect "0.6.1"]
-                      [prismatic/schema "1.1.10"]
-                      [selmer "1.12.12"]
-                      [hickory "0.7.1"]
-                      ]
-       :source-paths ["src" "../cryogen-core/src"]
+                      [cryogen-core "0.3.1"]]
        :plugins [[lein-ring "0.12.5"]]
        :main cryogen.core
        :ring {:init cryogen.server/init
